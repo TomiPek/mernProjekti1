61c332284db0436b0718bb26d6c6dcd01146e4ec
TomiPek <72078799+TomiPek@users.noreply.github.com>
2021-02-25 16:01:07 +0200

    Add files via upload
    päivitys

--- main/gb.js
+++ main/gb.js
@@ -1,35 +1,32 @@
 $(document).ready(function () {
-  $.getJSON(
-    "https://github.com/TomiPek/mernProjekti1/blob/main/main/data.json",
-    function (results) {
-      let data = results;
-      var taulukko = "<table>";
-      // Taululle otsikot
+  $.getJSON("data.json", function (results) {
+    let data = results;
+    var taulukko = "<table>";
+    // Taululle otsikot
+    taulukko +=
+      "<tr><td><h5>Name</h5></td><td><h5>Country</h5></td><td><h5>Message</h5></td><td><h5>Date</h5></td>";
+
+    for (var i = 0; i < data.length; i++) {
+      // datan läpikäynti ja tulostaminen sivulle
       taulukko +=
-        "<tr><td><h5>Name</h5></td><td><h5>Country</h5></td><td><h5>Message</h5></td><td><h5>Date</h5></td>";
-
-      for (var i = 0; i < data.length; i++) {
-        // datan läpikäynti ja tulostaminen sivulle
-        taulukko +=
-          "<tr>" +
-          "<td>" +
-          data[i].username +
-          "</td>" +
-          "<td>" +
-          data[i].country +
-          "</td>" +
-          "<td>" +
-          data[i].message +
-          "</td>" +
-          "<td>" +
-          data[i].date +
-          "</td>" +
-          "</tr>";
-      }
-
-      let print = document.getElementById("printloc");
-
-      print.innerHTML = taulukko;
+        "<tr>" +
+        "<td>" +
+        data[i].username +
+        "</td>" +
+        "<td>" +
+        data[i].country +
+        "</td>" +
+        "<td>" +
+        data[i].message +
+        "</td>" +
+        "<td>" +
+        data[i].date +
+        "</td>" +
+        "</tr>";
     }
-  );
+
+    let print = document.getElementById("printloc");
+
+    print.innerHTML = taulukko;
+  });
 });
